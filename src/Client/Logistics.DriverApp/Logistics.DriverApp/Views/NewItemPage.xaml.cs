12f6415448cf85fa84b6ade3e147f27b8714a9d6
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2021-11-27 15:40:20 -0500

    added base files

---
+++ src/Client/Logistics.DriverApp/Logistics.DriverApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Logistics.DriverApp.Models;
+using Logistics.DriverApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Logistics.DriverApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
---
+++ src/Server/Logistics.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Logistics.Domain;
+
+public class Class1
+{
+}
