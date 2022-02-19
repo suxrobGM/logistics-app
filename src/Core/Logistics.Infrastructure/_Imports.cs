abdb274dd7cf63413cb471636c4ff444cecd1376
Sukhrob Ilyosbekov <suxrobGM@gmail.com>
2022-02-19 14:57:02 -0500

    deleted old files

--- src/Core/Logistics.Infrastructure/_Imports.cs
+++
@@ -1,5 +0,0 @@
-﻿global using Logistics.Domain;
-global using Logistics.Domain.CargoAggregate;
-global using Logistics.Domain.UserAggregate;
-global using Logistics.Domain.TruckAggregate;
-global using Logistics.Infrastructure.Data;
