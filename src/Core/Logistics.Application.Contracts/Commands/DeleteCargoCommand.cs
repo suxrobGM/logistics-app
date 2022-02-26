d485c0c11d5f0c3ff57ad29566111a19a6037daa
Sukhrob Ilyosbekov <suxrobGM@gmail.com>
2022-02-26 01:00:49 -0500

    added delete commands

---
+++ src/Core/Logistics.Application.Contracts/Commands/DeleteCargoCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Logistics.Application.Contracts.Commands;
+
+public sealed class DeleteCargoCommand : RequestBase<DataResult>
+{
+    public string? Id { get; set; }
+}
---
+++ src/Core/Logistics.Application.Contracts/Commands/DeleteTruckCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Logistics.Application.Contracts.Commands;
+
+public sealed class DeleteTruckCommand : RequestBase<DataResult>
+{
+    public string? Id { get; set; }
+}
