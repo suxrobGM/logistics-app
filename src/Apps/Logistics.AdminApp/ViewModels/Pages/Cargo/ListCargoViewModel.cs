8d712af99577caa399e5c157c3b5041b539fb804
Sukhrob Ilyosbekov <suxrobGM@gmail.com>
2022-02-25 20:57:47 -0500

    added shared components

--- src/Apps/Logistics.AdminApp/ViewModels/Pages/Cargo/ListCargoViewModel.cs
+++ src/Apps/Logistics.AdminApp/ViewModels/Pages/Cargo/ListCargoViewModel.cs
@@ -6,5 +6,15 @@ public class ListCargoViewModel : PageViewModelBase
     public ListCargoViewModel(IApiClient apiClient)
         : base(apiClient)
     {
+        Cargoes = new PagedDataResult<CargoDto>();
     }
+
+    private PagedDataResult<CargoDto>? _cargoes;
+    public PagedDataResult<CargoDto>? Cargoes 
+    {
+        get => _cargoes;
+        set => SetProperty(ref _cargoes, value);
+    }
+
+
 }
