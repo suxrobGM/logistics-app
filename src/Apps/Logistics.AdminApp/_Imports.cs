4af4e0afa7c81401bde2a660df69fbbf8ee42907
Sukhrob Ilyosbekov <suxrobGM@gmail.com>
2022-03-03 15:52:19 -0500

    fixed compile errors

--- src/Apps/Logistics.AdminApp/_Imports.cs
+++ src/Apps/Logistics.AdminApp/_Imports.cs
@@ -1,7 +1,13 @@
 ﻿global using MagicMvvm;
+
+global using Logistics.Blazor;
+global using Logistics.Blazor.Buttons;
+global using Logistics.Blazor.Grids;
+global using Logistics.Blazor.Pagination;
+global using Logistics.Blazor.Popups;
+
 global using Logistics.Application.Contracts;
 global using Logistics.Application.Contracts.Models;
 global using Logistics.Application.Contracts.Commands;
 global using Logistics.Application.Contracts.Queries;
 global using Logistics.WebApi.Client;
-global using Logistics.Blazor.Components;
