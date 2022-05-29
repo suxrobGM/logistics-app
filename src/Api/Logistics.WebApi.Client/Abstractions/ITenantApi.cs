ffae7bb938715d711d703e02d8b18a822869a511
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-05-28 23:32:15 -0400

    name fix

--- src/Api/Logistics.WebApi.Client/Abstractions/ITenantApi.cs
+++ src/Api/Logistics.WebApi.Client/Abstractions/ITenantApi.cs
@@ -5,7 +5,7 @@ public interface ITenantApi
     Task<string> GetTenantDisplayNameAsync(string identifier);
     Task<TenantDto> GetTenantAsync(string identifier);
     Task<PagedDataResult<TenantDto>> GetTenantsAsync(string searchInput = "", int page = 1, int pageSize = 10);
-    Task CreateTenantAsync(TenantDto cargo);
-    Task UpdateTenantAsync(TenantDto cargo);
+    Task CreateTenantAsync(TenantDto tenant);
+    Task UpdateTenantAsync(TenantDto tenant);
     Task DeleteTenantAsync(string id);
 }
