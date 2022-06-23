4f4e976820f14b5b983dbb607b8df86ce63acaec
ketchapp99@gmail.com <ketchapp99@gmail.com>
2022-06-23 11:54:35 +0500

    copy files upder Manage folder

---
+++ src/Apps/Logistics.IdentityServer/Pages/Account/Manage/Email/InputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Logistics.IdentityServer.Pages.Account.Manage.Email
+{
+
+    public class InputModel
+{
+        [Required]
+        [EmailAddress]
+        [Display(Name = "New email")]
+        public string NewEmail { get; set; }
+    }
+}
---
+++ src/Apps/Logistics.IdentityServer/Pages/Account/Manage/Index/InputModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Logistics.IdentityServer.Pages.Account.Manage.Index
+{
+    public class InputModel
+    {
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
