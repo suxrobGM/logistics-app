104218ce121cc1b2cde58c9e3eb9d4789c06dd13
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-06-24 19:28:40 -0400

    added basic files for the SelectTenant and AddEmployee pages

---
+++ src/Apps/Logistics.OfficeApp/ViewModels/Pages/Employee/AddEmployeeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Logistics.OfficeApp.ViewModels.Pages;
+
+public class AddEmployeeViewModel : PageViewModelBase
+{
+    public AddEmployeeViewModel(IApiClient apiClient) : base(apiClient)
+    {
+        
+    }
+}
---
+++ src/Apps/Logistics.OfficeApp/ViewModels/Pages/SelectTenantViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Logistics.OfficeApp.ViewModels.Pages;
+
+public class SelectTenantViewModel : PageViewModelBase
+{
+    public SelectTenantViewModel(IApiClient apiClient) : base(apiClient)
+    {
+        
+    }
+}
