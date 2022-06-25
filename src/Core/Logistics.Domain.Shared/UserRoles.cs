64afaf860f423d4b699aa49a78841878e529b932
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-06-24 20:03:29 -0400

    removed unused user manager role from UserRoles

--- src/Core/Logistics.Domain.Shared/UserRoles.cs
+++ src/Core/Logistics.Domain.Shared/UserRoles.cs
@@ -5,7 +5,6 @@ public static class UserRoles
     public static IEnumerable<EnumType> GetValues()
     {
         yield return new EnumType("guest", "Guest");
-        yield return new EnumType("manager", "Manager");
         yield return new EnumType("admin", "Admin");
     }
 }
