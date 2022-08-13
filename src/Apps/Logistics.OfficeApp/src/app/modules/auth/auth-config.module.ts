47f6c724c9137fd26a6a6300da4797efa230ef1c
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-08-12 22:49:31 -0400

    fix tab size

--- src/Apps/Logistics.OfficeApp/src/app/modules/auth/auth-config.module.ts
+++ src/Apps/Logistics.OfficeApp/src/app/modules/auth/auth-config.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { AuthModule } from 'angular-auth-oidc-client';
 
 @NgModule({
-    imports: [AuthModule.forRoot({
-        config: {
-            authority: 'https://localhost:7001',
-            postLoginRoute: '/dashboard',
-            forbiddenRoute: '/forbidden',
-            unauthorizedRoute: '/unauthorized',
-            redirectUrl: window.location.origin,
-            postLogoutRedirectUri: window.location.origin,
-            clientId: 'logistics.officeapp',
-            scope: 'openid profile offline_access roles logistics.api.tenant',
-            responseType: 'code',
-            silentRenew: true,
-            useRefreshToken: true,
-            renewTimeBeforeTokenExpiresInSeconds: 30,
-        }
-    })],
-    exports: [AuthModule],
+  imports: [
+    AuthModule.forRoot({
+      config: {
+        authority: 'https://localhost:7001',
+        postLoginRoute: '/dashboard',
+        forbiddenRoute: '/forbidden',
+        unauthorizedRoute: '/unauthorized',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: 'logistics.officeapp',
+        scope: 'openid profile offline_access roles logistics.api.tenant',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        renewTimeBeforeTokenExpiresInSeconds: 30,
+      }
+    })
+  ],
+  exports: [AuthModule],
 })
 export class AuthConfigModule { }
