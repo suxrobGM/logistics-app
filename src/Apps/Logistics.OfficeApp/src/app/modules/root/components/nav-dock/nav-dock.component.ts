98abef0354d95c5edb7ba8655f327fdba0091652
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-09-19 17:41:57 -0400

    improved nav dock

--- src/Apps/Logistics.OfficeApp/src/app/modules/root/components/nav-dock/nav-dock.component.ts
+++ src/Apps/Logistics.OfficeApp/src/app/modules/root/components/nav-dock/nav-dock.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-nav-dock',
   templateUrl: './nav-dock.component.html',
-  styleUrls: ['./nav-dock.component.scss']
+  styleUrls: ['./nav-dock.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavDockComponent implements OnInit {
   dockItems!: MenuItem[];
