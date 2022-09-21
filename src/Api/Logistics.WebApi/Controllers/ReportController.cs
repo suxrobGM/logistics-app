91b5f7839c88958c19c3441e8061c14af07708bb
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2022-09-20 20:21:26 -0400

    added report controller

---
+++ src/Api/Logistics.WebApi/Controllers/ReportController.cs
@@ -0,0 +1,41 @@
+﻿namespace Logistics.WebApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ReportController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("getForInterval")]
+    [ProducesResponseType(typeof(DataResult<GrossesForIntervalDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(DataResult), StatusCodes.Status400BadRequest)]
+    [Authorize(Policy = Policies.Employee.CanRead)]
+    public async Task<IActionResult> GetGrossesForInterval([FromQuery] GetGrossesForIntervalQuery request)
+    {
+        var result = await _mediator.Send(request);
+
+        if (result.Success)
+            return Ok(result);
+
+        return BadRequest(result.Error);
+    }
+    
+    [HttpGet("getForTruck")]
+    [ProducesResponseType(typeof(DataResult<TruckGrossesDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(DataResult), StatusCodes.Status400BadRequest)]
+    [Authorize(Policy = Policies.Employee.CanRead)]
+    public async Task<IActionResult> GetGrossesForTruck([FromQuery] GetTruckGrossesForIntervalQuery request)
+    {
+        var result = await _mediator.Send(request);
+
+        if (result.Success)
+            return Ok(result);
+
+        return BadRequest(result.Error);
+    }
+}
