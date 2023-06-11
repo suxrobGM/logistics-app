bd4f51107889442591de75694b871c1775c0f74e
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-06-10 20:23:01 -0700

    added auditable entity

---
+++ src/Core/Logistics.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Logistics.Domain.Common;
+
+public abstract class AuditableEntity : Entity
+{
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
