615407be3f2af07754f366061d9db710a4906993
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-06-13 20:53:49 -0700

    namespace fix

--- src/Client/Logistics.AdminApp/_Imports.cs
+++ src/Client/Logistics.AdminApp/_Imports.cs
@@ -1,8 +1,6 @@
 ﻿global using Microsoft.AspNetCore.Components;
 
 global using Logistics.Blazor;
-global using Logistics.Blazor.Buttons;
-global using Logistics.Blazor.Grids;
 global using Logistics.Blazor.Pagination;
 global using Logistics.Blazor.Popups;
 global using Logistics.Shared.Policies;
