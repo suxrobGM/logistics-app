332896224c99ff0e6d071d506a6b3f19da636813
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-09-06 03:00:32 -0400

    removed firebase sdk key

--- src/Core/Logistics.Application.Tenant/Services/PushNotificationService.cs
+++ src/Core/Logistics.Application.Tenant/Services/PushNotificationService.cs
@@ -7,20 +7,31 @@ namespace Logistics.Application.Tenant.Services;
 
 public class PushNotificationService : IPushNotificationService
 {
-    private readonly FirebaseApp _firebaseApp;
+    private readonly FirebaseApp? _firebaseApp;
     private readonly ILogger<PushNotificationService> _logger;
 
     public PushNotificationService(ILogger<PushNotificationService> logger)
     {
         _logger = logger;
-        _firebaseApp = FirebaseApp.Create(new AppOptions()
+
+        if (File.Exists("firebase-adminsdk-key.json"))
         {
-            Credential = GoogleCredential.FromFile("firebase-adminsdk-key.json")
-        });
+            _firebaseApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("firebase-adminsdk-key.json")
+            });
+        }
+        else
+        {
+            _firebaseApp = null;
+        }
     }
     
     public async Task SendNotificationAsync(string title, string body, string deviceToken)
     {
+        if (_firebaseApp is null)
+            return;
+        
         try
         {
             var message = new Message
