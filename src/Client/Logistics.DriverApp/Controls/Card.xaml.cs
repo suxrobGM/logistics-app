143e8549f652a82c9d8fd885a0aa1726986cda2f
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-09-27 03:46:43 -0400

    added custom card control, enhanced UI

---
+++ src/Client/Logistics.DriverApp/Controls/Card.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Logistics.DriverApp.Controls;
+
+public partial class Card : Frame
+{
+    public Card()
+    {
+        BackgroundColor = new Color(245, 245, 245);
+        // BorderColor = Colors.Gray;
+        CornerRadius = 10;
+        Padding = 10;
+        HasShadow = true;
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty TitleProperty =
+        BindableProperty.Create(nameof(Title), typeof(string), typeof(Card), string.Empty);
+    
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+}
