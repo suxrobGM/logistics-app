a5bf105f10f1fe782b5bc7de776aae844da921a0
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-11-27 21:58:00 -0500

    added subscription entity

---
+++ src/Core/Logistics.Domain/Entities/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using Logistics.Domain.Core;
+
+namespace Logistics.Domain.Entities;
+
+public class Subscription : AuditableEntity
+{
+    public required string TenantId { get; set; }
+    public virtual required Tenant Tenant { get; set; }
+    public bool IsTrial { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
