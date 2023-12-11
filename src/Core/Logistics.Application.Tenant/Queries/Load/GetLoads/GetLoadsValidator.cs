355ee501f733e0a1e3c2d6025e762425c6c432be
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2023-12-10 19:46:04 -0500

    minor fix

--- src/Core/Logistics.Application.Tenant/Queries/Load/GetLoads/GetLoadsValidator.cs
+++ src/Core/Logistics.Application.Tenant/Queries/Load/GetLoads/GetLoadsValidator.cs
@@ -6,12 +6,8 @@ internal sealed class GetLoadsValidator : AbstractValidator<GetLoadsQuery>
 {
     public GetLoadsValidator()
     {
-        RuleFor(i => i.Page)
-            .GreaterThanOrEqualTo(0);
-        
-        RuleFor(i => i.PageSize)
-            .GreaterThanOrEqualTo(1);
-        
+        RuleFor(i => i.Page).GreaterThanOrEqualTo(0);
+        RuleFor(i => i.PageSize).GreaterThanOrEqualTo(1);
         RuleFor(i => i.StartDate).LessThan(i => i.EndDate);
     }
 }
