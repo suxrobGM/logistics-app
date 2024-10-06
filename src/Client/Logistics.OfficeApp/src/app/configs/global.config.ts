738c07be07f63e06381591b2c2c4478ac7e6bdf5
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-10-06 03:55:57 -0400

    added app.config file

---
+++ src/Client/Logistics.OfficeApp/src/app/configs/global.config.ts
@@ -0,0 +1,12 @@
+import {environment} from "src/environments/environment";
+
+export const APP_CONFIG = {
+  apiHost: environment.apiHost,
+  idHost: environment.idHost,
+  mapboxToken: environment.mapboxToken,
+  storage: {
+    keys: {
+      user: "User",
+    },
+  },
+};
