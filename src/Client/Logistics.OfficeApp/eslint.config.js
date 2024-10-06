a8e4a625be20d2892796c6146b4ea0bd177f3e64
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-10-06 02:51:53 -0400

    added flat config eslint

---
+++ src/Client/Logistics.OfficeApp/eslint.config.js
@@ -0,0 +1,42 @@
+import angular from "angular-eslint";
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
