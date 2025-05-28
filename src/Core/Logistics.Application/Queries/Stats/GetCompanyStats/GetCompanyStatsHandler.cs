e8bb05bcf9ccbdf76f58f5c743f93517ee225f25
joe_z <joe_z@DESKTOP-0C9CSB5>
2025-05-28 16:43:14 +0400

    adding try cathc for sql function execution

--- src/Core/Logistics.Application/Queries/Stats/GetCompanyStats/GetCompanyStatsHandler.cs
+++ src/Core/Logistics.Application/Queries/Stats/GetCompanyStats/GetCompanyStatsHandler.cs
@@ -18,59 +18,65 @@ internal sealed class GetCompanyStatsHandler : RequestHandler<GetCompanyStatsQue
     protected override async Task<Result<CompanyStatsDto>> HandleValidated(
         GetCompanyStatsQuery req, CancellationToken cancellationToken)
     {
-        //var companyStats = new CompanyStatsDto();
+        var companyStats = new CompanyStatsDto();
 
-        //var rolesDict = (await _tenantUow.Repository<TenantRole>().GetListAsync()).ToDictionary(i => i.Name);
-        //var ownerRoleId = rolesDict[TenantRoles.Owner].Id;
-        //var managerRoleId = rolesDict[TenantRoles.Manager].Id;
-        //var dispatcherRoleId = rolesDict[TenantRoles.Dispatcher].Id;
-        //var driverRoleId = rolesDict[TenantRoles.Driver].Id;
+        try
+        {
+            var query = "SELECT * FROM get_company_stats();";
+            var result = await _tenantUow.ExecuteRawSql<CompanyStatsDto>(query);
+            companyStats = result.FirstOrDefault() ?? new CompanyStatsDto();
+        }
+        catch
+        {
+            var rolesDict = (await _tenantUow.Repository<TenantRole>().GetListAsync()).ToDictionary(i => i.Name);
+            var ownerRoleId = rolesDict[TenantRoles.Owner].Id;
+            var managerRoleId = rolesDict[TenantRoles.Manager].Id;
+            var dispatcherRoleId = rolesDict[TenantRoles.Dispatcher].Id;
+            var driverRoleId = rolesDict[TenantRoles.Driver].Id;
 
-        //var employeeRepository = _tenantUow.Repository<Employee>();
-        //var ownerEmployee = await employeeRepository.GetAsync(e => e.EmployeeRoles.Any(er => er.RoleId == ownerRoleId));
+            var employeeRepository = _tenantUow.Repository<Employee>();
+            var ownerEmployee = await employeeRepository.GetAsync(e => e.EmployeeRoles.Any(er => er.RoleId == ownerRoleId));
 
-        //companyStats.OwnerName = ownerEmployee?.GetFullName();
-        //companyStats.EmployeesCount = await employeeRepository.CountAsync();
-        //companyStats.ManagersCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == managerRoleId));
-        //companyStats.DispatchersCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == dispatcherRoleId));
-        //companyStats.DriversCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == driverRoleId));
-        //companyStats.TrucksCount = await _tenantUow.Repository<Truck>().CountAsync();
+            companyStats.OwnerName = ownerEmployee?.GetFullName();
+            companyStats.EmployeesCount = await employeeRepository.CountAsync();
+            companyStats.ManagersCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == managerRoleId));
+            companyStats.DispatchersCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == dispatcherRoleId));
+            companyStats.DriversCount = await employeeRepository.CountAsync(e => e.EmployeeRoles.Any(er => er.RoleId == driverRoleId));
+            companyStats.TrucksCount = await _tenantUow.Repository<Truck>().CountAsync();
 
-        //var now = DateTime.UtcNow;
-        //var startOfWeek = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Monday);
-        //var lastWeekStart = startOfWeek.AddDays(-7);
-        //var startOfMonth = new DateTime(now.Year, now.Month, 1, 0, 0, 0, DateTimeKind.Utc);
-        //var lastMonthStart = startOfMonth.AddMonths(-1);
+            var now = DateTime.UtcNow;
+            var startOfWeek = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Monday);
+            var lastWeekStart = startOfWeek.AddDays(-7);
+            var startOfMonth = new DateTime(now.Year, now.Month, 1, 0, 0, 0, DateTimeKind.Utc);
+            var lastMonthStart = startOfMonth.AddMonths(-1);
 
-        //var loadsRepository = _tenantUow.Repository<Load>();
-        //var thisWeekLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, startOfWeek, now));
-        //var lastWeekLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, lastWeekStart, startOfWeek));
-        //var thisMonthLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, startOfMonth, now));
-        //var lastMonthLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, lastMonthStart, startOfMonth));
+            var loadsRepository = _tenantUow.Repository<Load>();
+            var thisWeekLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, startOfWeek, now));
+            var lastWeekLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, lastWeekStart, startOfWeek));
+            var thisMonthLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, startOfMonth, now));
+            var lastMonthLoads = loadsRepository.ApplySpecification(new FilterLoadsByDeliveryDate(null, lastMonthStart, startOfMonth));
 
-        //companyStats.ThisWeekGross = thisWeekLoads.Sum(l => l.DeliveryCost);
-        //companyStats.ThisWeekDistance = thisWeekLoads.Sum(l => l.Distance);
-        //companyStats.LastWeekGross = lastWeekLoads.Sum(l => l.DeliveryCost);
-        //companyStats.LastWeekDistance = lastWeekLoads.Sum(l => l.Distance);
-        //companyStats.ThisMonthGross = thisMonthLoads.Sum(l => l.DeliveryCost);
-        //companyStats.ThisMonthDistance = thisMonthLoads.Sum(l => l.Distance);
-        //companyStats.LastMonthGross = lastMonthLoads.Sum(l => l.DeliveryCost);
-        //companyStats.LastMonthDistance = lastMonthLoads.Sum(l => l.Distance);
+            companyStats.ThisWeekGross = thisWeekLoads.Sum(l => l.DeliveryCost);
+            companyStats.ThisWeekDistance = thisWeekLoads.Sum(l => l.Distance);
+            companyStats.LastWeekGross = lastWeekLoads.Sum(l => l.DeliveryCost);
+            companyStats.LastWeekDistance = lastWeekLoads.Sum(l => l.Distance);
+            companyStats.ThisMonthGross = thisMonthLoads.Sum(l => l.DeliveryCost);
+            companyStats.ThisMonthDistance = thisMonthLoads.Sum(l => l.Distance);
+            companyStats.LastMonthGross = lastMonthLoads.Sum(l => l.DeliveryCost);
+            companyStats.LastMonthDistance = lastMonthLoads.Sum(l => l.Distance);
 
-        //var totalStats = loadsRepository.Query()
-        //    .GroupBy(_ => 1).
-        //    Select(i => new
-        //    {
-        //        TotalDistance = i.Sum(m => m.Distance),
-        //        TotalGross = i.Sum(m => m.DeliveryCost)
-        //    }).FirstOrDefault();
+            var totalStats = loadsRepository.Query()
+                .GroupBy(_ => 1).
+                Select(i => new
+                {
+                    TotalDistance = i.Sum(m => m.Distance),
+                    TotalGross = i.Sum(m => m.DeliveryCost)
+                }).FirstOrDefault();
 
-        //companyStats.TotalDistance = totalStats?.TotalDistance ?? 0;
-        //companyStats.TotalGross = totalStats?.TotalGross ?? 0;
+            companyStats.TotalDistance = totalStats?.TotalDistance ?? 0;
+            companyStats.TotalGross = totalStats?.TotalGross ?? 0;
+        }
 
-        var query = "SELECT * FROM get_company_stats();";
-        var result = await _tenantUow.ExecuteRawSql<CompanyStatsDto>(query);
-
-        return Result<CompanyStatsDto>.Succeed(result.FirstOrDefault() ?? new CompanyStatsDto());
+        return Result<CompanyStatsDto>.Succeed(companyStats);
     }
 }
--- src/Core/Logistics.Application/Queries/Stats/GetTrucksStatsList/GetTruckStatsListHandler.cs
+++ src/Core/Logistics.Application/Queries/Stats/GetTrucksStatsList/GetTruckStatsListHandler.cs
@@ -19,57 +19,64 @@ public class GetTruckStatsListHandler : RequestHandler<GetTrucksStatsListQuery, 
     protected override async Task<PagedResult<TruckStatsDto>> HandleValidated(
         GetTrucksStatsListQuery req, CancellationToken cancellationToken)
     {
-        //var orderBy = req.OrderBy ?? string.Empty;
-        //var isDescendingOrder = false;
+        IEnumerable<TruckStatsDto> truckStatsDto = new List<TruckStatsDto>();
+        var totalItems = 0;
+        try
+        {
+            FormattableString query = $@"
+                SELECT * FROM get_trucks_stats(
+                    {req.StartDate}::timestamp,
+                    {req.EndDate}::timestamp,
+                    {req.Page},
+                    {req.PageSize},
+                    {req.OrderBy}
+                )";
+            truckStatsDto = await _tenantUow.ExecuteRawSql<TruckStatsDto>(query);
+            totalItems = truckStatsDto.FirstOrDefault()?.TotalItems ?? 0;
+        }
+        catch
+        {
+            var orderBy = req.OrderBy ?? string.Empty;
+            var isDescendingOrder = false;
 
-        //if (orderBy.StartsWith('-'))
-        //{
-        //    orderBy = orderBy[1..];
-        //    isDescendingOrder = true;
-        //}
+            if (orderBy.StartsWith('-'))
+            {
+                orderBy = orderBy[1..];
+                isDescendingOrder = true;
+            }
 
-        //var totalItems = await _tenantUow.Repository<Truck>().CountAsync();
-        //var truckStatsQuery = _tenantUow.Repository<Load>()
-        //    .ApplySpecification(new FilterLoadsByDeliveryDate(null, req.StartDate, req.EndDate))
-        //    .GroupBy(load => load.AssignedTruckId!)
-        //    .Select(group => new TruckStats
-        //    {
-        //        TruckId = group.Key!.Value,
-        //        TruckNumber = group.First().AssignedTruck!.TruckNumber,
-        //        Gross = group.Sum(load => load.DeliveryCost),
-        //        Distance = group.Sum(load => load.Distance),
-        //        DriverShare = group.Sum(load => load.DeliveryCost) * (decimal)group.First().AssignedTruck!.GetDriversShareRatio(),
-        //        Drivers = group.First().AssignedTruck!.Drivers,
-        //    });
+            totalItems = await _tenantUow.Repository<Truck>().CountAsync();
+            var truckStatsQuery = _tenantUow.Repository<Load>()
+                .ApplySpecification(new FilterLoadsByDeliveryDate(null, req.StartDate, req.EndDate))
+                .GroupBy(load => load.AssignedTruckId!)
+                .Select(group => new TruckStats
+                {
+                    TruckId = group.Key!.Value,
+                    TruckNumber = group.First().AssignedTruck!.TruckNumber,
+                    Gross = group.Sum(load => load.DeliveryCost),
+                    Distance = group.Sum(load => load.Distance),
+                    DriverShare = group.Sum(load => load.DeliveryCost) * (decimal)group.First().AssignedTruck!.GetDriversShareRatio(),
+                    Drivers = group.First().AssignedTruck!.Drivers,
+                });
 
 
-        //truckStatsQuery = truckStatsQuery
-        //    .OrderBy(InitOrderBy(orderBy), isDescendingOrder)
-        //    .ApplyPaging(req.Page, req.PageSize);
+            truckStatsQuery = truckStatsQuery
+                .OrderBy(InitOrderBy(orderBy), isDescendingOrder)
+                .ApplyPaging(req.Page, req.PageSize);
 
-        //var truckStatsDto = truckStatsQuery.ToArray()
-        //    .Select(result => new TruckStatsDto
-        //    {
-        //        TruckId = result.TruckId,
-        //        TruckNumber = result.TruckNumber,
-        //        StartDate = req.StartDate,
-        //        EndDate = req.EndDate,
-        //        Gross = result.Gross,
-        //        Distance = result.Distance,
-        //        DriverShare = result.DriverShare,
-        //        Drivers = result.Drivers.Select(i => i.ToDto())
-        //    });
-
-        FormattableString query = $@"
-    SELECT * FROM get_trucks_stats(
-        {req.StartDate}::timestamp,
-        {req.EndDate}::timestamp,
-        {req.Page},
-        {req.PageSize},
-        {req.OrderBy}
-    )";
-        var truckStatsDto = await _tenantUow.ExecuteRawSql<TruckStatsDto>(query);
-        var totalItems = truckStatsDto.FirstOrDefault()?.TotalItems ?? 0;
+            truckStatsDto = truckStatsQuery.ToArray()
+                .Select(result => new TruckStatsDto
+                {
+                    TruckId = result.TruckId,
+                    TruckNumber = result.TruckNumber,
+                    StartDate = req.StartDate,
+                    EndDate = req.EndDate,
+                    Gross = result.Gross,
+                    Distance = result.Distance,
+                    DriverShare = result.DriverShare,
+                    Drivers = result.Drivers.Select(i => i.ToDto()).ToList()
+                });
+        }
 
         return PagedResult<TruckStatsDto>.Succeed(truckStatsDto, totalItems, req.PageSize);
     }
