389a7b8204951f1c3172f4edafc402075d7d331f
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-16 02:29:52 -0500

    fixed tms portal runtime errors

--- src/Client/Logistics.Angular/projects/tms-portal/src/environments/environment.prod.ts
+++ src/Client/Logistics.Angular/projects/tms-portal/src/environments/environment.prod.ts
@@ -5,10 +5,10 @@ export const environment = {
   // mapboxToken: "${MAPBOX_TOKEN}",
   // stripePublishableKey: "${STRIPE_PUBLISHABLE_KEY}",
 
-  mapboxToken:
-    "pk.eyJ1Ijoic3V4cm9iZ20iLCJhIjoiY2w0dmsyMGd1MDEzZDNjcXcwZGRtY255MSJ9.XwGTNZx_httMhW0Fu34udQ",
   apiUrl: "https://api.suxrobgm.net",
   identityServerUrl: "https://id.suxrobgm.net",
+  mapboxToken:
+    "pk.eyJ1Ijoic3V4cm9iZ20iLCJhIjoiY2w0dmsyMGd1MDEzZDNjcXcwZGRtY255MSJ9.XwGTNZx_httMhW0Fu34udQ",
   stripePublishableKey:
     "pk_test_51QyIsE4Klqx5siJt8eAQrn9CxmrqRxgnvV2GfZIVmWClrofjDZYcFqcvPlYsxImxegjVxgPZciYc1bqs8veooAE2002gsEpsit",
 };
--- src/Client/Logistics.Angular/projects/tms-portal/src/environments/environment.ts
+++ src/Client/Logistics.Angular/projects/tms-portal/src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:7000/api",
-  identityServerUrl: "https://localhost:7001",
-  mapboxToken: "",
-  stripePublishableKey: "",
+  apiUrl: "http://localhost:7000",
+  identityServerUrl: "http://localhost:7001",
+  mapboxToken:
+    "pk.eyJ1Ijoic3V4cm9iZ20iLCJhIjoiY2w0dmsyMGd1MDEzZDNjcXcwZGRtY255MSJ9.XwGTNZx_httMhW0Fu34udQ",
+  stripePublishableKey:
+    "pk_test_51QyIsE4Klqx5siJt8eAQrn9CxmrqRxgnvV2GfZIVmWClrofjDZYcFqcvPlYsxImxegjVxgPZciYc1bqs8veooAE2002gsEpsit",
 };
