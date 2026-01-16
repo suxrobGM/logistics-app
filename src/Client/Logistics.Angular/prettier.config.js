c52c3a1ec90c175d8488435b95831a79d1ab6063
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-16 01:12:25 -0500

    moved OfficeApp files into tms-portal project

---
+++ src/Client/Logistics.Angular/prettier.config.js
@@ -0,0 +1,30 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100, // max 100 chars in line, code is easy to read
+  useTabs: false, // use spaces instead of tabs
+  tabWidth: 2, // "visual width" of of the "tab"
+  trailingComma: "all", // add trailing commas in objects, arrays, etc.
+  semi: true, // add ; when needed
+  singleQuote: false, // use double quotes
+  bracketSpacing: true, // import { some } from 'module' instead of import {some} from 'module'
+  arrowParens: "always", // braces even for single param in arrow functions (a) => { }
+  jsxSingleQuote: false, // "" for react props, like in html
+  bracketSameLine: false, // pretty JSX
+  endOfLine: "lf", // 'lf' for linux, 'crlf' for windows, we need to use 'lf' for git
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  importOrderParserPlugins: ["typescript", "decorators"],
+  importOrder: ["^(angular/(.*)$)|^(angular$)", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderSideEffects: false,
+  overrides: [
+    {
+      files: "*.html",
+      options: {
+        parser: "angular",
+      },
+    },
+  ],
+};
+
+export default config;
