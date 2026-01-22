a5366830321c57f0426a90b7b18c77a8c44121a2
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-21 20:37:33 -0500

    added the website marketing Angular project

---
+++ src/Client/Logistics.Angular/projects/website/eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+import { defineConfig } from "eslint/config";
+import rootConfig from "../../eslint.config.js";
+
+export default defineConfig([
+  ...rootConfig,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    rules: {},
+  }
+]);
---
+++ src/Client/Logistics.Angular/projects/website/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { type ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
+};
