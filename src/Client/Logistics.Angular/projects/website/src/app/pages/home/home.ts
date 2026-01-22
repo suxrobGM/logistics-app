cce69d5482ecc07d478177e5850a80eca1e3f683
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-01-22 00:31:30 -0500

    restructured home page section files

--- src/Client/Logistics.Angular/projects/website/src/app/pages/home/home.ts
+++ src/Client/Logistics.Angular/projects/website/src/app/pages/home/home.ts
@@ -1,13 +1,7 @@
 import { Component, signal } from "@angular/core";
 import { Footer, Navbar } from "@/layout";
 import { DemoDialog } from "@/shared/components";
-import { Faq } from "./sections/faq";
-import { Features } from "./sections/features";
-import { Hero } from "./sections/hero";
-import { HowItWorks } from "./sections/how-it-works";
-import { Integrations } from "./sections/integrations/integrations";
-import { Pricing } from "./sections/pricing";
-import { Testimonials } from "./sections/testimonials";
+import { Faq, Features, Hero, HowItWorks, Integrations, Pricing, Testimonials } from "./sections";
 
 @Component({
   selector: "web-home",
---
+++ src/Client/Logistics.Angular/projects/website/src/app/pages/home/sections/index.ts
@@ -0,0 +1,7 @@
+export * from "./faq/faq";
+export * from "./hero/hero";
+export * from "./integrations/integrations";
+export * from "./testimonials/testimonials";
+export * from "./features/features";
+export * from "./how-it-works/how-it-works";
+export * from "./pricing/pricing";
