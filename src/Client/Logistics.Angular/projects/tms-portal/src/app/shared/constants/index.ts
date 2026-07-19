4c6bdf23fb2470c6d9e0759a510a95f0ff8f0cb9
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-07-18 23:48:42 -0400

    refactor(tms): remove dead duplicate countries/us-states constants

--- src/Client/Logistics.Angular/projects/tms-portal/src/app/shared/constants/index.ts
+++ src/Client/Logistics.Angular/projects/tms-portal/src/app/shared/constants/index.ts
@@ -1,5 +1,3 @@
-export * from "./countries.options";
-export * from "./us-states.options";
 export * from "./regex-patterns";
 export * from "./financials-chart.options";
 export * from "./drivers-chart.options";
